0cffd6b70686438b1f427040ebe79d15e521f8cc
Richard <programmingislife23@gmail.com>
2024-07-09 15:23:44 -0300

    servidor rodando

---
+++ server.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config()
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`The server is running on PORT: ${PORT}`)
+})
+
