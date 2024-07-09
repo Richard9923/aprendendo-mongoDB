76d5f20f8315f0c78cb48e86bf99849cd57e213f
Richard <programmingislife23@gmail.com>
2024-07-09 16:29:11 -0300

    adicionando itens no banco de dados mongodb

--- models/product.model.js
+++ models/product.model.js
@@ -1,6 +1,5 @@
 const { default: mongoose } = require("mongoose");
 
-const mongoose = require(mongoose);
 const ProductSchema = mongoose.Schema(
     {
         name: {
--- server.js
+++ server.js
@@ -1,25 +1,42 @@
 
-require('dotenv').config()
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const { Admin } = require('mongodb');
+const Product = require('./models/product.model');
 const PORT = process.env.PORT || 3000;
 const password = process.env.password;
 const user = process.env.user;
+
+
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`The server is running on PORT: ${PORT}`)
-});
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Hello from node API');
 });
 
+app.post('/api/products', async (req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    try {
+      const product = await Product.create(req.body);
+      res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+})
+
 mongoose.connect(`mongodb+srv://${user}:${password}@backenddb.dgjbbik.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`)
 .then(() => {
     console.log("Connected to database!");
+    app.listen(PORT, () => {
+        console.log(`The server is running on PORT: ${PORT}`)
+    });
 })
 .catch(() => {
     console.log("Connection failed")
