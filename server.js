43f126e7e7cae362a3fadb6031a7f7f6aa434057
Richard <programmingislife23@gmail.com>
2024-07-09 18:24:16 -0300

    identificando somente um item pelo id

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ app.get('/api/products', async (req, res) => {
     }
 })
 
+app.get('/api/products/:id', async(req, res) => {
+
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
 app.post('/api/products', async (req, res) => {
     // console.log(req.body);
     // res.send(req.body);
