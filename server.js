bb56fbe63b273451f7b6f7c12d70166babdafe0c
Richard <programmingislife23@gmail.com>
2024-07-09 18:40:11 -0300

    adicionando delete na api

--- server.js
+++ server.js
@@ -71,6 +71,22 @@ app.put('/api/product/:id', async (req, res) => {
     }
 })
 
+app.delete('/api/product/id:', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const product = await Product.findByIdAndDelete(id);
+        
+        if (!product) {
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json({message: "Product deleted successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+    
+})
+
 
 
 mongoose.connect(`mongodb+srv://${user}:${password}@backenddb.dgjbbik.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`)
