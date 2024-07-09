75512c01fdd2dc68a90211b8cb20e06418a79285
Richard <programmingislife23@gmail.com>
2024-07-09 16:02:07 -0300

    conectando ao banco de dados mongodb

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ require('dotenv').config()
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const { Admin } = require('mongodb');
 const PORT = process.env.PORT || 3000;
+const password = process.env.password;
+const user = process.env.user;
 const app = express();
 
 app.listen(PORT, () => {
@@ -13,5 +16,13 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('Hello from node API');
 });
+
+mongoose.connect(`mongodb+srv://${user}:${password}@backenddb.dgjbbik.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`)
+.then(() => {
+    console.log("Connected to database!");
+})
+.catch(() => {
+    console.log("Connection failed")
+})
 
 
