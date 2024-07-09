2cacfd1e9386cb2c5e690ea75d8b50ba1ada47e4
Richard <programmingislife23@gmail.com>
2024-07-09 15:47:06 -0300

    importand mongoose

--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 require('dotenv').config()
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.listen(PORT, () => {
     console.log(`The server is running on PORT: ${PORT}`)
-})
+});
 
 app.get('/', (req, res) => {
     res.send('Hello from node API');
-})
+});
+
+
